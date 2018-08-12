49e88d4d44080671486ab8eb047fef35067b44a0
Alex Siu <atenthirtyone@gmail.com>
2018-08-12 22:14:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ Variables defined in the test suite:
 - oldID
 - currentAge
 
-let newID 
-
 You should NOT try to redefine any of the above variables.
 */
 
 // Write your code below this line!
+
+let newID = 
